61daf892db9343a77fd2fa0f204ec3009667080c
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-14 15:44:24 +0100

    ex00

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -1,31 +1,47 @@
 #pragma once
 #include <algorithm>
 #include <iostream>
+#include <stdexcept>
 
+class NotFoundException : public std::exception 
+{
+public:
+    virtual const char* what() const throw() 
+	{
+        return "Value not found in container";
+    }
+};
 
 template<typename T>
 typename T::iterator easyfind(T &container, int num)
 {
-	#ifdef DEBUG
-		std::cout << "non const easy find called" << std::endl;
-	#endif
+    #ifdef DEBUG
+        std::cout << "non const easy find called" << std::endl;
+    #endif
 
-	typename T::iterator it;
-
+    typename T::iterator it;
+	
 	it = std::find(container.begin(), container.end(), num);
-
-	return it;
+    
+    if (it == container.end()) 
+        throw NotFoundException();
+    
+    return it;
 }
-
 
 template<typename T>
 typename T::const_iterator easyfind(const T &container, int num)
 {
-	#ifdef DEBUG
-		std::cout << "const easy find called" << std::endl;
-	#endif
+    #ifdef DEBUG
+        std::cout << "const easy find called" << std::endl;
+    #endif
 
-	return std::find(container.begin(), container.end(), num);
+    typename T::const_iterator it;
+
+	it = std::find(container.begin(), container.end(), num);
+    
+    if (it == container.end())
+        throw NotFoundException();
+    
+    return it;
 }
-
-
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,55 +1,73 @@
-#include"easyfind.h"
+#include "easyfind.h"
 #include <vector>
 #include <cassert>
-
-typedef std::vector<int> vi;
-typedef std::vector<char> vch;
-
-
-template <typename T>
-void _display(T &container)
-{
-    typename T::iterator it;
-
-    for (it = container.begin(); it != container.end(); ++it)
-        std::cout << *it << std::endl;
-    
-}
+#include <iostream>
 
 
 void	_int()
 {
-	vi nums;
+	std::vector<int> nums;
 	nums.push_back(1337); nums.push_back(42); nums.push_back(19);
-	// _display(nums); exit(0);
 
 	assert (*easyfind(nums, 42) == 42);
 	assert (*easyfind(nums, 19) == 19);
 	assert (*easyfind(nums, 1337) == 1337);
-	assert(*easyfind(nums, 444) == *nums.end());
 
-	std::vector<int>::iterator itr = easyfind(nums, 888);
-	std::cout << *itr << std::endl;
+	try
+	{
+		easyfind(nums, 444);
+	} catch (const std::exception& e) 
+	{
+		std::cout << e.what() << std::endl;
+	}
 
-	*easyfind(nums, 19) = 1715;
+	try
+	{
+		std::vector<int>::iterator itr;
+		itr = easyfind(nums, 888);
+
+	} catch (const std::exception& e) 
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	// non const
+	std::vector<int>::iterator it = easyfind(nums, 19);
+	std::cout << "it = " << *it << std::endl;
+	*it = 1715;
+	std::cout << "it = " << *it << std::endl;
 	assert (*easyfind(nums, 1715) == 1715);
+}
 
+void	_const()
+{
+	std::vector<int> nums;
+	nums.push_back(1337); nums.push_back(42); nums.push_back(19);
 
-	const vi const_nums(nums);
+	const std::vector<int> const_nums(nums);
 	assert (*easyfind(const_nums, 1337) == 1337);
-	// *easyfind(const_nums, 19) = 1715; // assignment of read-only location
 
 	std::vector<int>::const_iterator it = easyfind(const_nums, 42);
 	std::cout << *it << std::endl;
+	// *it = 444; // assignment of read-only location 
+
+	try 
+	{
+		easyfind(const_nums, 999);
+
+	} catch (const std::exception& e) 
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 }
 
 void	_char()
 {
-	vch str;
+	std::vector<char> str;
 	str.push_back('h'); str.push_back('e'); str.push_back('j');
 
-	for (vch::iterator it = str.begin(); it != str.end(); ++it)
+	for (std::vector<char>::iterator it = str.begin(); it != str.end(); ++it)
 	{
 	    std::cout << *it << std::endl;
 	}
@@ -58,14 +76,24 @@ void	_char()
 	assert(*easyfind(str, 'e') == 'e');
 	assert(*easyfind(str, 'j') == 'j');
 
-}
+	try 
+	{
+		easyfind(str, 'x');
+	} catch (const std::exception& e) 
+	{
+		std::cout << e.what() << std::endl;
+	}
 
+	*easyfind(str, 'j') = 'k';
+	assert(*easyfind(str, 'k') == 'k');
+}
 
 int main(void)
 {
 	_int();
-
+	_const();
 	_char();
-
+	
 	return 0;
 }
+
